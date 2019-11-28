26dba7b6d13b4932378c31ed641df376cca1804d
nuria.aquilue <nuria.aquilue@ctfc.cat>
2019-11-28 14:02:01 +0100

    sdm climate RCP

--- mdl/read.climatic.vars.r
+++ mdl/read.climatic.vars.r
@@ -15,7 +15,8 @@ read.climatic.vars <- function(){
   ## List the name of the forest species
   species <- c("phalepensis", "pnigra", "ppinea", "psylvestris", "ppinaster", "puncinata",
                "aalba", "qilex", "qsuber", "qfaginea", "qhumilis", "fsylvatica", "other")
-
+  order.spp.sdm <- c(4,5,3,1,6,2,7,9,10,8,11,12,13)
+    
   ## Default extent of raster maps of Catalonia  
   extCat <- extent(c(250000, 540000, 4480000, 4760000))
   
@@ -25,40 +26,31 @@ read.climatic.vars <- function(){
       print(paste("Building:", clim.scn, "-", decade))
       
       ## Update annual minimum temp and annual precip
+      ## Change resolution and extend to match the default
       TEMP <- raster(paste0("inputlyrs/asc/", clim.scn, "/", decade, "/mnan.asc"))
-      TEMPcat <- setExtent(TEMP, extCat, keepres=T, snap=FALSE)
-      TEMP100 <- disaggregate(TEMPcat, fact=c(10,10))
+      TEMP <- disaggregate(TEMP, fact=c(10,10))
+      TEMP <- extend(TEMP, extCat)
       PRECIP <- raster(paste0("inputlyrs/asc/", clim.scn, "/", decade, "/plan.asc"))
-      PRECIPcat <- setExtent(PRECIP, extCat, keepres=T, snap=FALSE)
-      PRECIP100 <- disaggregate(PRECIPcat, fact=c(10,10))
-      rm(TEMP); rm(TEMPcat); rm(PRECIP); rm(PRECIPcat)
+      PRECIP <- disaggregate(PRECIP, fact=c(10,10))
+      PRECIP <- extend(PRECIP, extCat)
       
       ## Update list of SDMs
+      ## Change resolution and extend to match the default
       load(paste0("inputlyrs/rdata/SDM_", clim.scn, "_", decade, "_100m.rdata"))
-      
+      for(i in 1:length(sdm.proj)){
+        sdm.proj[[i]] <- disaggregate(sdm.proj[[i]], fact=c(10,10))
+        sdm.proj[[i]] <- extend(sdm.proj[[i]], extCat)
+      }
+        
       ## Build a data frame with MASK, TEMP, PRECIP and SDMs per spp
       clim <- data.frame(cell.id=1:ncell(MASK), mask=MASK[], temp=TEMP[], precip=PRECIP[])
-      for(spp in species){
-        SDM <- raster(paste0(mdl.path, "/inputlyrs/asc/SDM_", spp, "_", decade,"_100m.asc"))
-        clim <- cbind(clim, data.frame(spp=SDM[]))
-        names(clim)[ncol(clim)] <- spp
-      }
-      
-      ## Because som CAT cells have no value in TEMP layer, phacoyte
-      clim <- cbind(clim, coordinates(MASK))
-      # Find 8 neighbours for non-forest cells
-      nastemp <- filter(clim, mask==1, is.na(temp))
-      if(nrow(nastemp)>0){
-        neigh.id <- nn2(select(clim,x,y), filter(clim, mask==1, is.na(temp))%>%select(x,y),
-                        searchtype='priority', k=25)
-        neigh.id <-  neigh.id$nn.idx 
-        neigh.temp <- matrix(clim$temp[neigh.id[,-1]], nrow=nrow(neigh.id), ncol=ncol(neigh.id)-1) 
-        clim$temp[!is.na(clim$mask) & is.na(clim$temp)] <- apply(neigh.temp, 1 ,mean, na.rm=T)
-      }
+      for(i in order.spp.sdm)
+        clim <- cbind(clim, data.frame(spp=sdm.proj[[i]][]))
+      names(clim)[5:17] <- species
       
       ## Save SDM of all spp in a data.frame, for cells in CAT
       sdm <- clim[!is.na(clim$mask),]
-      sdm <- select(sdm, -mask, -temp, -precip, -x, -y) 
+      sdm <- select(sdm, -mask, -temp, -precip) 
       names(sdm)[-1] <- paste0("sdm.",species)
       sdm$sdm.shrub <- 1
       save(sdm, file=paste0("inputlyrs/rdata/sdm_", clim.scn, "_", decade, ".rdata"))
