92ffe21125dbb2ff0c03911fab2bfb9bb56a382d
nuria.aquilue <nuria.aquilue@ctfc.cat>
2021-02-08 16:14:42 +0100

    spp==14

--- mdl/post.fire.r
+++ mdl/post.fire.r
@@ -67,7 +67,7 @@ post.fire <- function(land, coord, orography, clim, sdm){
                            c_aspect*ifelse(aspect!=1,0,1)+c_slope*slope.stand) %>%
                   mutate(sq=1/(1+exp(-1*aux))) %>% mutate(sqi=ifelse(sq<=p50, 1, ifelse(sq<=p90, 2, 3))) %>%
                   select(cell.id, spp, temp, precip, biom, age, sdm, sqi)
-    sqi.shrub <- filter(clim, cell.id %in% shrub.cells) %>% select(spp, temp, precip) %>% 
+    sqi.shrub <- filter(clim, spp==14) %>% select(spp, temp, precip) %>% 
                  mutate(aux.brolla=site.quality.shrub$c0_brolla+site.quality.shrub$c_temp_brolla*temp+site.quality.shrub$c_temp2_brolla*temp*temp+site.quality.shrub$c_precip_brolla*precip+site.quality.shrub$c_precip2_brolla*precip*precip,
                         aux.maquia=site.quality.shrub$c0_maquia+site.quality.shrub$c_temp_maquia*temp+site.quality.shrub$c_temp2_maquia*temp*temp+site.quality.shrub$c_precip_maquia*precip+site.quality.shrub$c_precip2_maquia*precip*precip,
                         aux.boix=site.quality.shrub$c0_boix+site.quality.shrub$c_temp_boix*temp+site.quality.shrub$c_temp2_boix*temp*temp+site.quality.shrub$c_precip_boix*precip+site.quality.shrub$c_precip2_boix*precip*precip,
